b8008be97ef04d2c36e221112a7b69881da1ae06
JonathanLNB <dj.jlnp@gmail.com>
2018-10-11 00:34:11 -0500

    🎈 Proyecto base

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var path = require('path');
+var router = express.Router();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var pg = require('pg');
 var app = express();
 var bodyParser = require('body-parser');
-//Ejemplo: var direccion = "postgres://xico:fsd74126@localhost:5432/xico";
+//Ejemplo: var direccion = "postgres://usuario:contraseña@localhost:5432/basededatos";
 var cliente, query;
 
 /*
@@ -16,11 +17,11 @@ var cliente, query;
  |___|_| |_| |_| .__/ \___/|_|   \__\__,_|_|     \____|_|\__,_|___/\___||___/
                |_|                                                           
   Ejemplo:
-  var indexRouter = require('./routes/Alumnos/index');
+  const {notFound, errorHandler} = require('./routes/Middleware');
 */
 var app = express();
 
-const {notFound, errorHandler} = require('./routes/Middleware')
+const {notFound, errorHandler} = require('./routes/Middleware');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,7 +35,8 @@ app.use(cookieParser());
  |_____|_|\__,_|_| |_| |_|\__,_|_|    |_| |_| |_|\___|\__\___/ \__,_|\___/ |___/
 
 Ejemplo
- //app.use('/', indexRouter);
+ router.post('/', funcion);
+ router.get('/', funcion);
 */
 
 app.use(notFound);

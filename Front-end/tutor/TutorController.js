4fbed91b5ee291c667aa1577d6159ed4c8707f95
alan <15030093@itcelaya.edu.mx>
2018-11-27 20:01:05 -0600

    vista alumno

--- Front-end/tutor/TutorController.js
+++ Front-end/tutor/TutorController.js
@@ -1,25 +1,76 @@
+window.onload=llenadoMensaje();
+
+function llenadoMensaje() {
+    txtA1=document.getElementById('mensaje1');
+    txtA2=document.getElementById('mensaje2');
+    txtA3=document.getElementById('mensaje3');
+    if(localStorage.getItem('mensaje1')!=null)
+        txtA1.value=localStorage.getItem('mensaje1');
+    if(localStorage.getItem('mensaje2')!=null)
+        txtA2.value=localStorage.getItem('mensaje2');
+    if(localStorage.getItem('mensaje3')!=null)
+        txtA3.value=localStorage.getItem('mensaje3')
+
+}
+
 function GroupCards() {
     console.log("entre al metodo");
     let cards;
     let colapsible;
-    for(i=0;i<4;i++){
-        cards='<div class="card">\n' +
-            '                            <div class="card-header" id="headingOne">\n' +
-            '                                <h5 class="mb-0">\n' +
-            '                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse'+i+'" aria-expanded="true" aria-controls="collapse'+i+'">\n' +
-                                                  'Card'+i+
-            '                                    </button>\n' +
-            '                                </h5>\n' +
-            '                            </div>\n' +
-            '\n' +
-            '                            <div id="collapse'+i+'" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">\n' +
-            '                                <div class="card-body">\n' +
-            '                                    Esta es la carta '+i+'\n' +
-            '                                </div>\n' +
-            '                            </div>\n' +
-            '                        </div>'//fin de cards
-        colapsible=document.getElementById("accordionExample");
-        colapsible.insertAdjacentHTML("afterend", cards)
-    }
+}
 
+function enviar1() {
+    let textarea=document.getElementById('mensaje1');
+    let mensaje=textarea.value;
+    localStorage.setItem('1','hola');
+    if(localStorage.getItem('mensaje1')==null){
+        console.log("entre al primer if");
+        localStorage.setItem('mensaje1',mensaje);
+        alert('mensaje enviado correctamente');
+        textarea.value=mensaje;
+        console.log(localStorage.getItem('mensaje1'));
+    }
+    else {
+        console.log("else");
+        localStorage.setItem('mensaje1',mensaje);
+        textarea.value=mensaje;
+        console.log(localStorage.getItem('mensaje1'));
+        alert('mensaje enviado correctamente');
+
+    }
+}
+
+function enviar2(){
+    let textarea=document.getElementById('mensaje2');
+    let mensaje=textarea.value;
+    if(localStorage.getItem('mensaje2')==null){
+        console.log("entre al primer if");
+        console.log("este es el segundo if");
+        localStorage.setItem('mensaje2',mensaje);
+        textarea.value=mensaje;
+        console.log(localStorage.getItem('mensaje1'));
+        alert('mensaje enviado correctamente')
+    }
+    else {
+        localStorage.setItem('mensaje2',mensaje);
+        alert('mensaje enviado correctamente');
+        textarea.value=mensaje;
+    }
+}
+
+function enviar3() {
+    let textarea=document.getElementById('mensaje3');
+    let mensaje=textarea.value;
+    if(localStorage.getItem('mensaje3')==null){
+        console.log("entre al primer if");
+        localStorage.setItem('mensaje3',mensaje);
+        textarea.value=mensaje;
+        alert('mensaje enviado correctamente')
+    }
+    else {
+        localStorage.setItem('mensaje3',mensaje);
+        textarea.value=mensaje;
+        alert('mensaje enviado correctamente');
+
+    }
 }
